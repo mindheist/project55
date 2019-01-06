c7f0295dba7aefc2250d85e540495db6dd6bebac
Prabhu Dhanapal <dragonzsnake@gmail.com>
2019-01-05 17:29:52 -0800

    added an employee list array

--- src/app/employee-name/employee-name.component.ts
+++ src/app/employee-name/employee-name.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeNameComponent implements OnInit {
 
+  public employees = [
+    { "id":1 , "name" : "Andrew" , "age":25},
+    { "id":2, "name": "Jason", "age":30}
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
