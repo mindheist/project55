8c262f8af5122463acee09b68d7f68a7ebb50144
Prabhu Dhanapal <dragonzsnake@gmail.com>
2019-01-05 19:38:44 -0800

    removed the hard coded employee list from the employee service ( since we are going to use HTTP)

--- src/app/employee-list.service.ts
+++ src/app/employee-list.service.ts
@@ -9,12 +9,5 @@ export class EmployeeListService {
   constructor(private http: HttpClient) { }
 
   getEmployees(){
-    return  [
-      { "id":1,  "name": "Andrew", "age":25},
-      { "id":2,  "name": "Jason",  "age":30},
-      { "id":3,  "name": "Barbara","age":40},
-      { "id":4,  "name": "Chad",   "age":50},
-      { "id":5,  "name": "David",  "age":28}
-    ];
   }
 }
